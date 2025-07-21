26d8ce3fa1dddbbf814418be066eb5151359b545
KekoFigueroa-dev <thenaturalistgospel@gmail.com>
2025-07-21 14:23:57 -0600

    Core functionalities built

--- coin_flip_app.py
+++ coin_flip_app.py
@@ -29,9 +29,6 @@ while True:
         except Exception as e:
             print(f"An error occurred!: {e}")
 
-print(flip_number)
-print(choice)
-
 # --- Initialized Counters for Heads and Tails ---
 heads_count = 0
 tails_count = 0
@@ -44,8 +41,29 @@ tails_count = 0
 #   - If the user wants, display the result of this flip
 #   - Check if heads and tails counts are equal and print a message if so
 
+for flips in range(flip_number):
+    coin = random.randint(0,1)
+
+    if coin == 1:
+        heads_count += 1
+        if choice.startswith('y'):
+            print("HEADS")
+    else:
+        tails_count += 1
+        if choice.startswith('y'):
+            print("TAILS")
+    
+if heads_count == tails_count:
+    print(f"At {flips + 1} flips, the number of heads and tails were equal at {str(heads_count)}, each.")
+
 # --- Calculate and Print Flip Statistics ---
 # After the loop, calculate the percentage of heads and tails (rounded to two decimal places)
-# Print a summary showing:
-#   - Total heads and tails
-#   - Percentage results
+heads_percentage = round(100 * heads_count / flip_number, 2)
+tails_percentage = round(100 * tails_count / flip_number, 2)
+# Print a summary
+print(f"\nResults of Flipping a Coin {flip_number} Times: ")
+print(f"\nSide\t\tCount\tPercentage: ")
+print(f'Heads\t\t{str(heads_count)}/{str(flip_number)}\t\t{str(heads_percentage)}%')
+print(f'Tails\t\t{str(tails_count)}/{str(flip_number)}\t\t{str(tails_percentage)}%\n')
+
+
